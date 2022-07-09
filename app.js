4eb1df90619a3eed24d892e0c10a826282af369f
aleffstampini <aleff.stampini@bandtec.com.br>
2022-07-09 12:51:49 -0300

    criando rotas e adicionando conexão com database

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const express = require('express')
+const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running...')
+    console.log('[SERVER] Server is running...');
 });
 
-app.get('/', (req, res) => {
-    res.send('aaaaaaaaaaaaaaaaaaa');
+const conn = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '1601',
+    database: 'hltv',
+    insecureAuth: true
 });
+
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log('[DATABASE] Database connected!');
+});
+
+const team = require('./src/routes/team.js');
+const player = require('./src/routes/player.js');
+const championship = require('./src/routes/championship.js');
+const news = require('./src/routes/news.js');
+
+app.use('/team', team);
+app.use('/player', player);
+app.use('/championship', championship);
+app.use('/news', news);
